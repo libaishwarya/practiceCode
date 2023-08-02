177211291d2a687f71d902685447f8891006dfb9
Aishwarya <libaishwarya@gmail.com>
2023-08-02 12:36:17 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,108 @@
+import mysql.connector
+con = mysql.connector.connect(host="localhost",user="root", password="PASSWORD",database="CRUD")
+
+def insert():
+    Name = input("Enter the name: ")
+    Email = input("Enter the emailID: ")
+    Phone = input("Enter the phone number: ")
+    
+    res = con.cursor()
+    sql ="insert into students(name,email,phone) values(%s,%s,%s)"
+    res.execute(sql,(Name,Email,Phone))
+    con.commit()
+    print("\n")
+    print("Student data inserted")
+    
+def select():
+    res = con.cursor()
+    sql = "select name,email,phone from students WHERE id = %s "
+    id = int(input("Enter the ID: "))
+    res.execute(sql,(id,))
+    result = res.fetchone()
+    con.commit()
+    print(result)
+    
+    # if result:
+    #     print(result[0])
+    #     print(result[1])
+    #     print(result[2])
+
+def update():
+    res = con.cursor()
+    sql = "select name from students WHERE id = %s "
+    id = int(input("Enter the ID: "))
+    # sql = "UPDATE students SET name = %s WHERE id = %s;"
+    # id = int(input("Enter the Id to be changed: "))
+    res.execute(sql,[id,])
+    result = res.fetchone()
+    con.commit()
+    
+    def updateName():
+        res = con.cursor()
+        sql = "UPDATE students SET name = %s WHERE id = %s;"
+        id = int(input("Enter the Id to be changed: "))
+        name = str(input("Enter the name to be changed: "))
+        res.execute(sql,(name,id))
+        result = res.fetchall()
+        con.commit()
+        print(result)
+        
+    def updateEmail():
+        res = con.cursor()
+        sql = "UPDATE students SET email = %s WHERE id = %s;"
+        id = int(input("Enter the Id to be changed: "))
+        email = str(input("Enter the emailID to be changed: "))
+        res.execute(sql,(email,id))
+        result = res.fetchall()
+        con.commit()
+        print(result)
+        
+    def updatePhone():
+        res = con.cursor()
+        sql = "UPDATE students SET phone = %s WHERE id = %s;"
+        id = int(input("Enter the Id to be changed: "))
+        phone = str(input("Enter the phone number to be changed: "))
+        res.execute(sql,(phone,id))
+        result = res.fetchall()
+        con.commit()
+        print(result)
+    
+    if result:
+        print("1. Update Name")
+        print("2. Update Email")
+        print("3. Update Phone")
+        print("4. Exit")
+        print("\n")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            updateName()
+        elif choice == 2:
+            updateEmail()
+        elif choice == 3:
+            updatePhone()
+        else:
+            quit()
+        
+while True:
+        print('\n')
+        print("1. Insert Record")
+        print("2. Select Record")
+        print("3. Update Record")
+        print("4. Delete Record")
+        print("5. Exit")
+        print("\n")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            insert()
+        elif choice == 2:
+            select()
+        elif choice == 3:
+            update()
+        # elif choice == 4:
+        #     delete()
+        elif choice == 5:
+            quit()
+        else:
+            print("Invalid choice")
+            
+        
---
+++ schema.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for macos13 (x86_64)
+--
+-- Host: localhost    Database: CRUD
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `email` varchar(20) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (1,'Kavya','kavya@gmail.com','9876543210'),(2,'Aishwarya','aishu@gmail.com','9840091135'),(3,'Mohan','MPS@gmail.com','8220556865'),(4,'mohan','mohan@mohan.com','7200'),(5,'aishu','aishu@aishu.com','9876'),(6,'test','test@test.com','67855');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'CRUD'
+--
+
+--
+-- Dumping routines for database 'CRUD'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-02 12:28:04
